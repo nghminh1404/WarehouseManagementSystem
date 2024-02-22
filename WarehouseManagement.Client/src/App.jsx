e16d6b4947177248a4f014ef62d7af625ac445c2
haiquang12 <quanghai12112001@gmail.com>
2024-02-22 20:57:39 +0700

    create client

---
+++ WarehouseManagement.Client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Login from './view/pages/authentication/Login'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Login/>
+    </>
+  )
+}
+
+export default App
