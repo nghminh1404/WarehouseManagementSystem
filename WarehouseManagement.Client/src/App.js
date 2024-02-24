625a00d0b914078bd04c324352c789d365ff98dd
Trần Xuân Sơn <sowndev@gmail.com>
2024-02-24 15:11:21 +0700

    add router

--- WarehouseManagement.Client/src/App.js
+++ WarehouseManagement.Client/src/App.js
@@ -1,9 +1,19 @@
 import Login from './views/pages/authentication/Login';
 import ForgotPassword from './views/pages/authentication/ForgotPassword';
 import ResetPassword from './views/pages/authentication/ResetPassword';
-
+import { Routes, Route } from 'react-router-dom';
+import NavbarCom from './views/components/NavbarCom';
 function App() {
-    return <ResetPassword />;
+    return (
+        <div className="app">
+            <NavbarCom />
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/reset-password" element={<ResetPassword />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
