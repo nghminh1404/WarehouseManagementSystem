e2c6e5472028fc06c03f902f394c95d0233415a8
nghminh1404 <nguyenhoangminh614@gmail.com>
2024-03-16 10:24:03 +0700

    Add crud filter import order
    chưa có chức năng nhập hàng: thay đổi số lượng goods khi xác nhận nhập hàng thành công

---
+++ WarehouseManagement.Server/WM.Entity/DTOs/ImportOrderDTO/CreateImportOrderRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WM.Entity.DTOs.ImportOrderDTO
+{
+    public class CreateImportOrderRequest
+    {
+        public int UserId { get; set; }
+
+        public int SupplierId { get; set; }
+       
+        public float TotalCost { get; set; }
+
+        public string? Note { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? ImportedDate { get; set; }
+
+        public int StatusId { get; set; }
+       
+        public string ImportCode { get; set; } = null!;
+
+        public int StorageId { get; set; }
+       
+        public int? ProjectId { get; set; }
+       
+        public int? DeliveryId { get; set; }
+       
+        public string? Image { get; set; }
+
+        public int? StorekeeperId { get; set; }
+        public List<ImportDetailDTO> ImportOrderDetails { get; set; }
+
+    }
+}
---
+++ WarehouseManagement.Server/WM.Entity/DTOs/ImportOrderDTO/ImportDetailDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WM.Entity.DTOs.ImportOrderDTO
+{
+    public class ImportDetailDTO
+    {
+        public int ImportId { get; set; }
+
+        public float CostPrice { get; set; }
+
+        public int DetailId { get; set; }
+
+        public int? GoodsId { get; set; }
+
+        public string? GoodsName { get; set; }
+
+        public string? MeasureUnit { get; set; }
+
+        public int? Quantity { get; set; }
+    }
+}
